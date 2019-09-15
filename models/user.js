b560096fb98535213868bfeb4ced0ce69d2d63fc
yuanwentian <yuanwentian97@gmail.com>
2019-09-15 16:53:46 -0700

    iter0 database async

--- models/user.js
+++ models/user.js
@@ -5,4 +5,44 @@ var userSchema = new mongoose.Schema({
     password: String
 });
 
-module.exports = mongoose.model("User", userSchema);
+
+
+userSchema.statics.userExists = async function(username) {
+    return User.exists({username: username});
+}
+ 
+userSchema.statics.getOneUserByUsername = async function(username) {
+    return User.findOne({username: username}, function(err, newlyFoundUser){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("found User" + newlyFoundUser);
+        }
+    });
+}
+
+userSchema.statics.removeOneUserByUsername = async function(username) {
+    return User.remove({username: username}, function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            //console.log("remove");
+        }
+    })
+}
+
+
+
+userSchema.statics.addOneUser = async function(userObject) {
+    User.create(userObject, function(err, user){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("added" + user + "to the user collections");
+        }
+    });
+}
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User;
