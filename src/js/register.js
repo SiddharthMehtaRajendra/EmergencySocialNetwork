14e13ec4683857a51c1690864e8be75c2838e01f
soulze <729872933@qq.com>
2019-09-16 19:03:12 -0700

    add handler for jumping to register from home

--- src/js/register.js
+++ src/js/register.js
@@ -7,6 +7,7 @@ function initRegisterHandler() {
         axios.get(`${SERVER_ADDRESS}${API_PREFIX}/test`).then((res) => {
             console.log(res);
             document.getElementById('api-result').innerText = JSON.stringify(res.data);
+            // jump to another web
         })
     })
 }
