ab1a44ad91bfc258183631d0c67ac77fe3bebfef
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro-2.local>
2019-09-17 16:31:40 -0700

    Fixed Code

--- src/js/register.js
+++ src/js/register.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
-import { validateUserName, validatePassword } from './validation'
-import { SERVER_ADDRESS, API_PREFIX } from './constant'
-import * as util from './appUtils'
+import { validateUserName, validatePassword } from './validation';
+import { SERVER_ADDRESS, API_PREFIX } from './constant';
 
 function initRegisterHandler() {
-    let registerBtn = document.getElementById('register-btn');
-    let $username = document.getElementById('username')
-    let $password = document.getElementById('password')
+    const registerBtn = document.getElementById('register-btn');
+    const $username = document.getElementById('username');
+    const $password = document.getElementById('password');
     registerBtn.addEventListener('click', (e) => {
-        $username.insertAdjacentHTML('afterend', "")
-        $password.insertAdjacentHTML('afterend', "")
-        let userValidation = validateUserName($username.value)
-        let passwordValidation = validatePassword($password.value)
+        const userValidation = validateUserName($username.value);
+        const passwordValidation = validatePassword($password.value);
         if (userValidation.result !== false && passwordValidation.result !== false) {
             axios.post(`${SERVER_ADDRESS}${API_PREFIX}/registerUser`, {
                 username: $username.value,
@@ -20,21 +17,17 @@ function initRegisterHandler() {
                 console.log(res);
                 document.getElementById('api-result').innerText = JSON.stringify(res.data);
             }).catch((err) => {
-                console.log(err)
-            })
+                console.log(err);
+            });
         } else {
-            let html = document.createElement('div')
-            html.className = "error-register"
-            if(util.isNotEmpty(userValidation.text)){
-                html.innerText = userValidation.text
-                $username.insertAdjacentHTML('afterend', html)
-            } 
-            if(util.isNotEmpty(passwordValidation.text)) {
-                html.innerText = passwordValidation.text
-                $password.insertAdjacentHTML('afterend', html)
+            if (userValidation && userValidation.text) {
+                /* Append Username Errors Here* TODO: Yi Wang */
+            }
+            if (passwordValidation && passwordValidation.text) {
+                /* Append Passwords Error Here* TODO: Yi Wang */
             }
         }
-    })
+    });
 }
 
-export default initRegisterHandler
+export default initRegisterHandler;
