0df705073877e897f85000b9f85a62bc370108ca
yiwang <yiwang3@andrew.cmu.edu>
2019-09-18 13:39:52 -0700

    fix

--- server/connectdb.js
+++ server/connectdb.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const User = require("../models/user");
-
 const url = "mongodb://127.0.0.1/SB2ESN";
 const connectDB = mongoose.connect(url, {useNewUrlParser: true});
 
