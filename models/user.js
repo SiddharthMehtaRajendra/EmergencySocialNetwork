ca1d2c001cb26ecb3fd47424142f436e646d8dae
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro-2.local>
2019-09-18 00:07:09 -0700

    Encryption and Server Validation

--- models/user.js
+++ models/user.js
@@ -1,8 +1,65 @@
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 var userSchema = new mongoose.Schema({
     username: String,
     password: String
+    // acknowledgeStatus: {type: Boolean, default: false},
+    // acknowledgedAt: {type: Date, default: Date.now}
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.statics.userExists = async function (username) {
+    return await User.exists({ username: username });
+};
+
+userSchema.statics.getOneUserByUsername = async function (username) {
+    return await User.findOne({ username: username }, function (err, newlyFoundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('found User' + newlyFoundUser);
+        }
+    });
+};
+
+userSchema.statics.removeOneUserByUsername = async function (username) {
+    return await User.remove({ username: username }, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            // console.log("remove");
+        }
+    });
+};
+
+userSchema.statics.addOneUser = async function (userObject) {
+    User.create(userObject, function (err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('added' + user + 'to the user collections');
+        }
+    });
+};
+
+userSchema.statics.validateCredentials = async function (username, password) {
+    const user = await User.findOne({username: username});
+    if(!user){
+        return false;
+    } else {
+        const isMatch = await bcrypt.compare(password, password);
+        return isMatch;
+    }
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
-let express = require('express');
-let app = express();
-let cors = require('cors');
-let bodyParser = require('body-parser');
-let http = require('http').createServer(app);
+const express = require('express');
+const serverValidation = require('./server-validation');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const http = require('http').createServer(app);
 const port = 8000;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/api/test', async function (req, res, next) {
     console.log('/api/test Get Request');
-    res.status(200).json({success: true, message: 'Test OK', data: "Test OK"})
+    res.status(200).json({ success: true, message: 'Test OK', data: 'Test OK' });
+});
+
+app.post('/api/register', async function (req, res, next) {
+    if(!await serverValidation.validateCredentials(req.body.username, req.body.password)){
+        res.status(401).json({ success: false, message: 'Invalid Credentials', data: 'Invalid Credentials' });
+    }
+    res.status(200).json({ success: true, message: 'Register OK', data: 'Register OK' });
 });
 
 http.listen(port, function () {
-    console.log(`Express server start, listening on port:${port} ...`)
+    console.log(`Express server start, listening on port:${port} ...`);
 });
