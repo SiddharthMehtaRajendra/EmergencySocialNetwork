61122178cf33b68a37c4d5a8944949548300ae61
yiwang <yiwang3@andrew.cmu.edu>
2019-09-26 20:37:24 -0700

    bottom navigation

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ import Welcome from './view/welcome.html';
 import Home from './view/home.html';
 import Error from './view/error.html';
 import Join from './view/join.html';
+import Me from './view/_me.html';
+import Directory from './view/_directory.html';
+import Chats from './view/_chats.html';
+import Announcement from './view/_announcement.html';
 import initRouter from './js/initRouter';
 import initJoinPage from './js/join';
+import initBottomTab from './components/bottomTab';
 
 initRouter();
+initBottomTab();
 
 const app = document.getElementById('app');
 const router = new Navigo(null, true, '#');
@@ -26,6 +32,22 @@ router.on('/join', function () {
 
 router.on('/welcome', function () {
     app.innerHTML = Welcome;
+}).resolve();
+
+router.on('/me', function () {
+    app.innerHTML = Me;
+}).resolve();
+
+router.on('/directory', function () {
+    app.innerHTML = Directory;
+}).resolve();
+
+router.on('/chats', function () {
+    app.innerHTML = Chats;
+}).resolve();
+
+router.on('/announcement', function () {
+    app.innerHTML = Announcement;
 }).resolve();
 
 router.notFound(function () {
