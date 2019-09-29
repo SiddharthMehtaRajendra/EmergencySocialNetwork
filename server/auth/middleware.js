22887b1ec5e192692cda5f162021eeee320e6034
soulze <729872933@qq.com>
2019-09-29 14:48:13 -0700

    debug

--- server/auth/middleware.js
+++ server/auth/middleware.js
@@ -1,33 +1,41 @@
 const jwt = require('jsonwebtoken');
 const config = require('./config.js');
 
-function checkToken (req, res, next) {
-  let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-  if (token.startsWith('Bearer ')) {
-    // Remove Bearer from string
-    token = token.slice(7, token.length);
-  }
-
-  if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) {
-        return res.json({
-          success: false,
-          message: 'Token is not valid'
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.json({
-      success: false,
-      message: 'Auth token is not supplied'
-    });
-  }
+const exclude = {
+    '/api/joinCheck': true,
+    '/api/join': true
 };
 
-module.exports = {
-  checkToken: checkToken
-}
+const checkToken = (req, res, next) => {
+    console.log(req.originalUrl);
+    if (exclude[req.originalUrl]) {
+        next();
+    } else {
+        // const token = req.headers['x-access-token'] || req.headers.authorization; // Express headers are auto converted to lowercase
+        console.log(req.cookies);
+        const token = req.cookies && req.cookies.token;
+        if (token) {
+            jwt.verify(token, config.secret, (err, decoded) => {
+                if (err) {
+                    return res.json({
+                        success: false,
+                        message: 'Token is not valid'
+                    });
+                } else {
+                    req.decoded = decoded;
+                    console.log(decoded);
+                    // note, the exp time here is 10 digit, not 13!
+                    next();
+                }
+            });
+        } else {
+            // TODO: Use res.location to redirect
+            return res.json({
+                success: false,
+                message: 'Auth token is not supplied'
+            });
+        }
+    }
+};
+
+module.exports = checkToken;
