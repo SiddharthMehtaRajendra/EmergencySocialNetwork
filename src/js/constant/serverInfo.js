f0218a6f316ba8e47e84efe4f53fed0f0fd3c5f0
yiwang <yiwang3@andrew.cmu.edu>
2019-10-01 03:22:36 -0700

    fix ip

--- src/js/constant/serverInfo.js
+++ src/js/constant/serverInfo.js
@@ -1,3 +1,3 @@
-export const SERVER_ADDRESS = 'http://192.168.2.160:8000';
+export const SERVER_ADDRESS = 'http://localhost:8000';
 // Replace the ip with your own pc ip, and then run it on your mobile phone!
 export const API_PREFIX = '/api';
