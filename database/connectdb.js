4fc0f18b267d2b031557a0612e2cf3ef4e1baace
yiwang <yiwang3@andrew.cmu.edu>
2019-10-01 21:15:55 -0700

    for deploy

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1/SB2ESN';
+// const url = 'mongodb://127.0.0.1/SB2ESN';
+// { user: "heroku_n79vmv8t", account: "heroku_n79vmv8t",password:"f19sb2no1" }
+// const url = 'mongodb://heroku_n79vmv8t:heroku_n79vmv8t@ds029824.mlab.com:29824/heroku_n79vmv8t';
+const url = process.env.MONGODB_URI;
+console.log(url);
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('connected', function () {
     console.log('Mongoose connection open to ' + url);
