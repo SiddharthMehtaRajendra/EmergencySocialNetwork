fd6adc40b2aebaa7862f2c28d701b7ba2b2091b3
Siddharth Mehta <mehtasiddharth1991@gmail.com>
2019-10-09 13:59:56 -0700

    Circle CI + Express

--- client/js/constant/serverInfo.js
+++ client/js/constant/serverInfo.js
@@ -1,3 +1,3 @@
-export const SERVER_ADDRESS = 'http://localhost:80';
+export const SERVER_ADDRESS = '';
 // Replace the ip with your own pc ip, and then run it on your mobile phone!
 export const API_PREFIX = '/api';
