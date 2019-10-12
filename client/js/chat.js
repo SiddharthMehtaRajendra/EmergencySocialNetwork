d4e964b5e0a456f23acb0abf1662a9a85d312390
soulze <729872933@qq.com>
2019-10-11 17:25:31 -0700

    fix chat publicly bug.
    Co-authored-by: YIWANG3 <yiwang3@andrew.cmu.edu>

--- client/js/chat.js
+++ client/js/chat.js
@@ -10,6 +10,8 @@ const pageSize = 20;
 
 async function getHistoryMessage() {
     window.state.isLoading = true;
+    console.log(window.state.user.username);
+    console.log(window.location.href.split('/').pop());
     const res = await axios.get(`${SERVER_ADDRESS}${API_PREFIX}/historyMessage`, {
         params: {
             smallestMessageId: window.state.smallestMessageId,
@@ -156,9 +158,7 @@ async function render() {
     window.state.isLoading = false;
     window.state.bubbleIsBottom = true;
     window.state.showMessageTip = false;
-    if (window.location.hash.indexOf('public') >= 0) {
-        document.getElementById('single-chat-navbar-title').innerText = 'Public Wall';
-    }
+    document.getElementById('single-chat-navbar-title').innerText = window.location.href.split('/').pop();
     document.getElementById('navbar-back-arrow').addEventListener('click', function () {
         window.history.go(-1);
     });
