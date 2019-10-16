65dd677b4cabcd95c8017f0e8ec02bae373ef45e
YIWANG3 <yiwang3@andrew.cmu.edu>
2019-10-16 01:50:43 -0700

    fix unit test

--- tests/unit/model_chat.test.js
+++ tests/unit/model_chat.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const Chat = require('../../database/model/Chat');
 
-describe('Chat DB Test', () => {
+describe('Chat DB Test', async () => {
     const oneMessage = {
         time: new Date(),
         from: 'user1',
@@ -22,39 +22,33 @@ describe('Chat DB Test', () => {
         read: false,
         chatId: -1
     };
-    describe('Add Chat', async () => {
-        const oneChat = {
-            type: 'private',
-            from: 'user1',
-            to: 'user2',
-            latestMessage: oneMessage
-        };
-        test('test add chat', async () => {
-            const resultObj = (await Chat.insertOne(oneChat)).res;
-            const chatId = resultObj.chatId;
-            expect(resultObj).toEqual(expect.objectContaining(oneChat));
-            const getResult = (await Chat.getByChatId(chatId)).res[0];
-            expect(getResult).toEqual(expect.objectContaining(oneChat));
-            const updateResult = (await Chat.updateLatestMessage(chatId, newMessage)).res;
-            expect(updateResult.latestMessage).toEqual(expect.objectContaining(newMessage));
-        });
+    const oneChat = {
+        type: 'private',
+        from: 'user1',
+        to: 'user2',
+        latestMessage: oneMessage
+    };
+    test('test add chat', async () => {
+        const resultObj = (await Chat.insertOne(oneChat)).res;
+        const chatId = resultObj.chatId;
+        expect(resultObj).toEqual(expect.objectContaining(oneChat));
+        const updateResult = (await Chat.updateLatestMessage(chatId, newMessage)).res;
+        expect(updateResult.latestMessage).toEqual(expect.objectContaining(newMessage));
     });
 
-    describe('Related Chat', async () => {
-        const userName = 'testUser';
-        const chatList = [];
-        for (let i = 0; i < 5; i++) {
-            chatList.push({
-                type: 'private',
-                from: userName,
-                to: `user${i}`,
-                latestMessage: oneMessage
-            });
-            await Chat.insertOne(chatList[i]);
-        }
-        test('test add chat', async () => {
-            const related = (await Chat.related(userName)).res;
-            expect(related.length).toEqual(chatList.length);
+    const userName = 'testUser';
+    const chatList = [];
+    for (let i = 0; i < 5; i++) {
+        chatList.push({
+            type: 'private',
+            from: userName,
+            to: `user${i}`,
+            latestMessage: oneMessage
         });
+        await Chat.insertOne(chatList[i]);
+    }
+    test('test related chat', async () => {
+        const related = (await Chat.related(userName)).res;
+        expect(related.length).toEqual(chatList.length);
     });
 });
