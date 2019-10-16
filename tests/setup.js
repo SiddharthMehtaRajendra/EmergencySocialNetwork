7493a9dd4beda58018e09f6954837810a9547eff
YIWANG3 <yiwang3@andrew.cmu.edu>
2019-10-15 19:48:50 -0700

    fix

--- tests/setup.js
+++ tests/setup.js
@@ -1,24 +1,25 @@
 /* eslint-disable no-undef */
 const mongoose = require('mongoose');
-const TestDB = 'mongodb+srv://f19sb2test:f19sb2test1234@cluster0-hfvai.mongodb.net/tests?retryWrites=true&w=majority';
+const TestDbUrl = 'mongodb+srv://f19sb2test:f19sb2test1234@cluster0-hfvai.mongodb.net/tests?retryWrites=true&w=majority';
+require('../database/model/User');
+require('../database/model/Chat');
+require('../database/model/Message');
 
 beforeAll(async () => {
-    async function clearDB() {
-        await Promise.all(
-            Object.keys(mongoose.connection.collections).map(async key => {
-                return mongoose.connection.collections[key].remove({});
-            })
-        );
-    }
+    console.log('Before All');
+    await mongoose.connect(TestDbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
-    if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(TestDB, {
-            useNewUrlParser: true
-        });
-    }
-    await clearDB();
+    await Promise.all(
+        Object.keys(mongoose.connection.collections).map(async key => {
+            return mongoose.connection.collections[key].remove({});
+        })
+    );
 });
 
 afterAll(async () => {
     await mongoose.disconnect();
+    console.log('After All');
 });
