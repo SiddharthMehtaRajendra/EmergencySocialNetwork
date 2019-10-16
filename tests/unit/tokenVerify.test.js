1841fbd807c422638ee725ea492b96b30449d35f
soulze <729872933@qq.com>
2019-10-16 10:49:44 -0700

    tokenParsingTest
    Co-authored-by: YIWANG3 <yiwang3@andrew.cmu.edu>

--- tests/unit/tokenVerify.test.js
+++ tests/unit/tokenVerify.test.js
@@ -1,15 +1,3 @@
-/* eslint-disable no-undef */
-const temp = require('../../client/js/lib/validation');
-
-describe('Example', () => {
-    describe('First', () => {
-        test('Example Test', () => {
-            expect('abc').toEqual('abc');
-            expect(temp.validatePassword('12312').result).toEqual(true);
-        });
-    });
-});
-
 const processMsg = require('../../server/index');
 
 test('messageParsing', () => {
