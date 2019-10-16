240789d997dbce15ebe39ea69e9d7951d043bdf9
soulze <729872933@qq.com>
2019-10-15 21:08:58 -0700

    processMsgTest

--- server/index.js
+++ server/index.js
@@ -296,4 +296,4 @@ http.listen(port, function () {
 function sum(a, b) {
     return a + b;
 }
-module.exports = sum;
+module.exports = processMsg;
--- server/server.test.js
+++ server/server.test.js
@@ -1,5 +1,12 @@
-const sum = require('./index');
+const processMsg = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('messageParsing', () => {
+    const obj = { from: '1111', to: '2222', type: '0', content: '234', status: '1', chatId: '2' };
+    const expected = { chatId: '2', content: '234', from: '1111', status: '1', to: '2222', type: '0' };
+    expect(processMsg(obj).chatId).toBe(expected.chatId);
+    expect(processMsg(obj).from).toBe(expected.from);
+    expect(processMsg(obj).content).toBe(expected.content);
+    expect(processMsg(obj).status).toBe(expected.status);
+    expect(processMsg(obj).type).toBe(expected.type);
+    expect(processMsg(obj).to).toBe(expected.to);
 });
