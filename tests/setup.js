a0e979d0a10118ab820c2d3444c64dd8c8f399fa
YIWANG3 <yiwang3@andrew.cmu.edu>
2019-10-19 01:47:04 -0700

    fix

--- tests/setup.js
+++ tests/setup.js
@@ -6,21 +6,26 @@ require('../database/model/Message');
 require('../database/model/Chat');
 
 beforeAll(async () => {
-    const DB_URL = `mongodb+srv://f19sb2test:f19sb2test1234@cluster0-hfvai.mongodb.net/${process.env.TEST_DB}?retryWrites=true&w=majority`;
-    await mongoose.connect(DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    if (process.env.TEST_DB) {
+        console.log(`Connect to ${process.env.TEST_DB}`);
+        const DB_URL = `mongodb+srv://f19sb2test:f19sb2test1234@cluster0-hfvai.mongodb.net/${process.env.TEST_DB}?retryWrites=true&w=majority`;
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-    await Promise.all(
-        Object.keys(mongoose.connection.collections).map(async key => {
-            if (key !== 'counter') {
-                return mongoose.connection.collections[key].remove({});
-            }
-        })
-    );
+        await Promise.all(
+            Object.keys(mongoose.connection.collections).map(async key => {
+                if (key !== 'counters') {
+                    return mongoose.connection.collections[key].remove({});
+                }
+            })
+        );
+    }
 });
 
 afterAll(async () => {
-    await mongoose.disconnect();
+    if (process.env.TEST_DB) {
+        await mongoose.disconnect();
+    }
 });
