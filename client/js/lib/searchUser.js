e5181ef42b05f04b919edc581cea610066527ffe
yuanwen.tian <yuanwentian97@gmail.com>
2019-10-25 23:01:55 -0700

    add back arrow

--- client/js/lib/searchUser.js
+++ client/js/lib/searchUser.js
@@ -1,1 +1,12 @@
 import SearchUser from "../../view/searchUser.html";
+import "../style/searchUser.less";
+import axios from "axios";
+import { SERVER_ADDRESS, API_PREFIX } from "../constant/serverInfo";
+
+const maxUserNum = 9999;
+
+function addArrawBackListener() {
+    document.getElementsByClassName("navbar-back-arrow")[0].addEventListener("click", () => {
+        window.history.go(-1);
+    });
+}
