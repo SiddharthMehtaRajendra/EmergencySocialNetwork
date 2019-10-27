277b0fbdd9455629b7126984c02e06a403d97d4a
soulze <729872933@qq.com>
2019-10-26 18:13:39 -0700

    add integration test of message search

--- tests/unit/model_message.test.js
+++ tests/unit/model_message.test.js
@@ -24,6 +24,58 @@ describe("Message DB Test", async () => {
         resultObj.time = new Date(resultObj.time);
         expect(resultObj).toEqual(expect.objectContaining(oneMessage));
     });
+
+    const publicMessage = {
+        time: new Date(),
+        from: "test",
+        to: "public",
+        type: "0",
+        content: "Public Content",
+        status: "ok",
+        read: false,
+        chatId: -1,
+        id: 1
+    };  
+    test("test existed search public message", async () => {
+        await Message.insertOne(publicMessage);
+        const resultObj = JSON.parse(JSON.stringify((await Message.searchPublicMessage("Public", 9999, 10)).res));
+        resultObj[0].time = new Date(resultObj[0].time);
+        publicMessage.id = resultObj[0].id;
+        expect(resultObj[0]).toEqual(expect.objectContaining(publicMessage));
+    });
+    test("test non-existed search public message", async () => {
+        const resultObj = await Message.searchPublicMessage("non-existed", 9999, 10);
+        expect(resultObj.res.length).toEqual(0);
+    });
+    /*
+    test("test invalid search of public message", async () => {
+        const resultObj = await Message.searchPublicMessage("Public", "123", 10);
+        expect(resultObj).toEqual(true);
+    });
+    */
+
+    const privateMessage = {
+        time: new Date(),
+        from: "1111",
+        to: "2222",
+        type: "0",
+        content: "Private Content",
+        status: "ok",
+        read: false,
+        chatId: -1
+    };
+    test("test existed search private message", async () => {
+        await Message.insertOne(privateMessage);
+        const resultObj = JSON.parse(JSON.stringify((await Message.searchPrivateMessage("1111", "Private", 9999, 10)).res));
+        privateMessage.id = resultObj[0].id;
+        resultObj[0].time = new Date(resultObj[0].time);
+        expect(resultObj[0]).toEqual(expect.objectContaining(privateMessage));
+    });
+    test("test non-existed search private message", async () => {
+        const resultObj = await Message.searchPrivateMessage("public", 9999, 10);
+        expect(resultObj.res.length).toEqual(0);
+    });
+
     const messageList = [];
     const latestDate = new Date();
     for(let i = 0; i < 5; i++) {
@@ -47,4 +99,5 @@ describe("Message DB Test", async () => {
             expect(historyMessages[i].content).toEqual(`${i} th message`);
         }
     });
+
 });
